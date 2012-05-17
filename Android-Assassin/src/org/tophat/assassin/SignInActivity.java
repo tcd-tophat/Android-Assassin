0b4f814f3c137603b017f8876a3f40e89d4b688a
Kevin Bluett <kevin.bluett@gmail.com>
2012-05-17 17:47:14 +0100

    Added screen for #6

--- Android-Assassin/src/org/tophat/assassin/SignInActivity.java
+++ Android-Assassin/src/org/tophat/assassin/SignInActivity.java
@@ -12,7 +12,7 @@ public class SignInActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.login);
     }
     
     @Override
